0c4aefab46de2d6b9d80e9adf772c75bd7baafa7
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2022-12-02 16:10:06 +0100

    add route guards

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -23,17 +23,34 @@ const router = createRouter({
         {
             path: '/products/:id',
             component: ProductItem,
-            props: true
+            props: true,
+            beforeEnter: (to, from, next) => {
+                const id = to.params.id;
+                if (![1, 2, 3, 4].includes(Number(id))) next('/not-found');
+                else next();
+            }
         },
         {
             path: '/login',
-            component: LoginBox
+            component: LoginBox,
+            beforeEnter: (to, from, next) => {
+                const token = localStorage.getItem('token');
+                if (token) next('/products');
+                else next();
+            }
         },
         {
             path: '/:pathMatch(.*)*',
             component: NotFound
         }
     ]
+});
+
+// the global route guard
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token');
+    if (!token && to.path !== '/login') next('/login');
+    else next();
 });
 
 export default router;
